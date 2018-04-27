4d4e88e176a1254d4b4b9603f86d9385040e504c
Hanumanakumar <hanumankumar035@gmail.com>
2018-04-27 17:00:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ self.addEventListener(install, event=>{
 event.waitUntill(
 caches.open(v1).then(cache=>{
 return cache.addAll([
-'index.html'
+'index.html',
+'hanuman/hanuman.html',
+'hanuman/main.js'
 ]);
 });
 )
