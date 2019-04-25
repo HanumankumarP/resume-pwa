e96efc084f5df85ec39289f034b36ed802b27dba
tirumalasettyvk98 <46346207+tirumalasettyvk98@users.noreply.github.com>
2019-04-25 11:59:02 +0530

    Updates

---
+++ precache-manifest.c6c59bc32b631cd83e90beac4178bb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc7fd31028fda3b7ef8",
+    "url": "/HanumankumarP/hanuman/static/css/main.93f14dc7.chunk.css"
+  },
+  {
+    "revision": "8cc7fd31028fda3b7ef8",
+    "url": "/HanumankumarP/hanuman/static/js/main.a5444387.chunk.js"
+  },
+  {
+    "revision": "0de81b4ea32e36cfed85",
+    "url": "/HanumankumarP/hanuman/static/js/runtime~main.de7e82f7.js"
+  },
+  {
+    "revision": "3c764f355ca3f31c3815",
+    "url": "/HanumankumarP/hanuman/static/js/2.b44a9c0d.chunk.js"
+  },
+  {
+    "revision": "2a42d7b274bb96d8cc0976555277bea8",
+    "url": "/HanumankumarP/hanuman/static/media/profile.2a42d7b2.png"
+  },
+  {
+    "revision": "226a40bf28d3964cee1836a544cc919d",
+    "url": "/HanumankumarP/hanuman/index.html"
+  }
+];
